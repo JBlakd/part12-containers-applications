f2949085b7a0df67fcbc314e683d9a2c619febaf
Ivan Hu <ivanhu2246@gmail.com>
2022-08-03 23:47:29 +0800

    12.12 complete

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Todo } = require('../mongo')
+const { getAsync, setAsync } = require('../redis')
 const router = express.Router();
 
 /* GET todos listing. */
@@ -14,33 +15,57 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false
   })
-  res.send(todo);
+
+  const curCounter = await getAsync("added_todos")
+
+  await setAsync("added_todos", Number(curCounter) + 1)
+
+  res.send(todo)
 });
+
+/* GET statistics */
+router.get('/statistics', async (req, res) => {
+  const curCounter = await getAsync("added_todos")
+  res.json({ added_todos: curCounter })
+})
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
+  console.log('findByIdMiddleware id:', id)
   req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  if (!req.todo) {
+    return res.sendStatus(404)
+  }
 
   next()
 }
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete()
   res.sendStatus(200);
 });
 
-/* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+/* GET */
+singleRouter.get('/', async (req, res, next) => {
+  // console.log('data from middleware: ', req.todo)
+  res.json(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  if (req.body.hasOwnProperty('text')) {
+    req.todo.text = req.body.text
+  }
+
+  if (req.body.hasOwnProperty('done')) {
+    req.todo.done = (req.body.done.toLowerCase() === 'true') ? true : false
+  }
+
+  const updatedTodo = await req.todo.save(req.todo)
+  res.json(updatedTodo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
